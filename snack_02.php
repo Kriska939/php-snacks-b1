018db5f2326c85b7869994eac95bbdb8f1e55a8b
Kriska939 <cristinacorrado93@gmail.com>
2021-09-23 17:07:18 +0200

    added complete snack 2

--- snack_02.php
+++ snack_02.php
@@ -7,15 +7,34 @@ Stampare Nome, Cognome e la media dei voti di ogni alunno. */
 // DATA
 
 $students = [
-    ['name' =>'Maria', 'lastname' => 'Bianchi', 'voti' => [
+    ['name' =>'Maria', 'lastname' => 'Bianchi', 'grades' => [
         10, 5, 8, 6],],
-    ['name' =>'Carlo', 'lastname' => 'Verdi', 'voti' => [
+    ['name' =>'Carlo', 'lastname' => 'Verdi', 'grades' => [
         8, 5, 4, 3],],
-    ['name' =>'Paola', 'lastname' => 'Rossi', 'voti' => [
+    ['name' =>'Paola', 'lastname' => 'Rossi', 'grades' => [
         10, 10, 2, 6],],
-    ['name' =>'Sandro', 'lastname' => 'Neri', 'voti' => [
+    ['name' =>'Sandro', 'lastname' => 'Neri', 'grades' => [
             4, 4, 7, 9],],
 ];
 
-
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 2</title>
+</head>
+<body>
+    <ul>
+        <?php foreach($students as $student) {?>
+            <li>
+            <?php echo $student['name'] . '</br> '. $student['lastname']. '</br> '. ' Media dei voti: ' ?> 
+            <?php echo $mean = ceil( array_sum($student['grades']) / 4 ) ?>
+            </li>
+            <?php }?>
+    </ul>
+    </body>
+</html>
